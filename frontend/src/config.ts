89570ec70fb8b6b591eb0259b4995fd9f2cba1d3
denis0001-dev <denis0001.dev@ya.ru>
2025-08-19 22:13:05 +0300

    Fix bugs

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL: string = '/api';
-export const API_FULL_BASE_URL: string = "localhost:8301/api";
+export const API_FULL_BASE_URL: string = `${location.hostname}:8301/api`;
 export const PRODUCT_NAME: string = "FromChat";
