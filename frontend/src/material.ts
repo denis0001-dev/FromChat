5c868b2076978e26105088acf5cbb90bca510008
denis0001-dev <denis0001.dev@ya.ru>
2025-08-20 20:00:40 +0300

    Replace with Material lists

--- frontend/src/material.ts
+++ frontend/src/material.ts
@@ -1,6 +1,10 @@
 import 'mdui/components/tabs.js';
 import 'mdui/components/tab.js';
 import 'mdui/components/tab-panel.js';
+import 'mdui/components/list.js';
+import 'mdui/components/list-item.js';
+import 'mdui/components/list-subheader.js';
+
 import { setColorScheme } from 'mdui/functions/setColorScheme.js';
 
 setColorScheme("#91cef4");
